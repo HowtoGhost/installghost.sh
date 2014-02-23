3d83f30cc078b7f3b231fa639507372aa3b8e9aa
David Balderston <david@howtoinstallghost.com>
2014-02-22 23:18:24 -0800

    Roll Back

--- installGhost.sh
+++ installGhost.sh
@@ -16,15 +16,10 @@ if grep "Ubuntu" "/tmp/osversion.txt" > /dev/null; then
     echo "Ubuntu"
     apt-get -y update
     aptitude -y install build-essential zip
-    useradd ghost
-    apt-get install nginx
 elif grep "SMP" "/tmp/osversion.txt" > /dev/null; then
     echo "CentOS"
     yum -y update
     /usr/bin/yum -y groupinstall "Development Tools"
-    adduser ghost
-    sudo su -c 'rpm -Uvh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm'
-    /usr/bin/yum install nginx -y
 elif grep "Debian" "/tmp/osversion.txt" > /dev/null; then
     echo "Mac OS X"
 fi
@@ -48,49 +43,33 @@ echo "node installed"
 cd /tmp
 rm -rf $nodeversion
 
-######Download Ghost######
+######Download and install Ghost######
 mkdir -p /var/www
 cd /var/www/
 curl -L -O https://ghost.org/zip/ghost-latest.zip
 unzip -d ghost ghost-latest.zip
 rm ghost.zip
-chown -R ghost:ghost /var/www/ghost/
 cd ghost/
-
-######Install Nginx######
-
-echo "starting nginx"
-service nginx start
-chkconfig nginx on
-echo 'server { / location / { proxy_set_header X-Real-IP $remote_addr; proxy_set_header Host $http_host; proxy_pass http://127.0.0.1:2368; } }' > /etc/nginx/conf.d/virtual.conf
-service nginx restart
-echo "nginx complete"
-
-######Install PM2######
-/usr/local/bin/npm install pm2 -g
-
-######Switch to Ghost User######
-su - ghost
-cd /var/www/ghost/
-
-######Install Ghost######
 /usr/local/bin/npm install --production
 
 ######Edit the Config File######
-#sed -e 's/127.0.0.1/0.0.0.0/' -e 's/2368/80/' <config.example.js >config.js
+sed -e 's/127.0.0.1/0.0.0.0/' -e 's/2368/80/' <config.example.js >config.js
 
-######Run PM2######
-echo "starting pm2"
-/usr/local/bin/npm install pm2 -g
-NODE_ENV=production /usr/local/bin/pm2 start index.js --name ghost
-s/usr/local/bin/pm2 dump
+######Install Forever######
+/usr/local/bin/npm install -g forever
 
-if grep "Ubuntu" "/tmp/osversion.txt" > /dev/null; then
-    /usr/local/bin/pm2 startup ubuntu
-else 
-	/usr/local/bin/pm2 startup centos
-fi
+#####Setup Forever Start Script######
+echo "#!/bin/bash" >> /usr/local/bin/ghoststart.sh
+echo "export PATH=/usr/local/bin:$PATH" >> /usr/local/bin/ghoststart.sh
+echo "cd /var/www/ghost" >> /usr/local/bin/ghoststart.sh
+echo "export NODE_ENV=production" >> /usr/local/bin/ghoststart.sh
+echo "NODE_ENV=production /usr/local/bin/forever -a -l /var/log/ghost start --sourceDir /var/www/ghost index.js" >> /usr/local/bin/ghoststart.sh
+chmod 755 /usr/local/bin/ghoststart.sh
 
-sed -i '0,/USER=root/ s/USER=root/#USER=ghost/' /etc/init.d/pm2-init.sh
+######Create Startup Cron######
+echo "@reboot /usr/local/bin/ghoststart.sh" > mycron
+crontab mycron
+rm mycron
 
-echo "pm2 complete"
+######Start Ghost with Forever######
+sh /usr/local/bin/ghoststart.sh
